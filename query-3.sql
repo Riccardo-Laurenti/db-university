3b3c4225a1bf5dd7b8104a67acc76304cc2ec7a2
Riccardo-Laurenti <riccardo.laurenti95@gmail.com>
2023-07-31 15:16:42 +0200

    i add 3 and 4 query JOIN

--- query-3.sql
+++ query-3.sql
@@ -16,3 +16,22 @@ FROM `departments`
 JOIN `degrees` ON `departments`.`id` = `degrees`.`department_id`
 WHERE `departments`.`name` LIKE 'Dipartimento di Neuroscienze'
 AND `degrees`.`level` LIKE 'Magistrale';
+
+3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+SELECT `courses`.name AS 'corsi'
+FROM `courses`
+JOIN `course_teacher`ON `courses`.`id`= `course_teacher`.`course_id`
+JOIN `teachers` ON `course_teacher`.`teacher_id`= `teachers`.`id`
+where `teachers`.`id`= 44;
+
+
+4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui
+sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e
+nome
+
+SELECT `students`.`surname` AS 'Cognome',`students`.`name` as 'Nome', `degrees`.`name` AS 'Corso_di_laurea', `departments`.`name` AS 'Dipartimento'
+FROM `students`
+JOIN `degrees` ON `students`.`degree_id`= `degrees`.`id`
+JOIN `departments`ON `degrees`.`department_id`= `departments`.`id`
+ORDER BY `students`.`surname`,`students`.`name`;
